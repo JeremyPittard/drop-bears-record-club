1dfe74945619e48cb11a6aea2fe5b37212a8d583
Jeremy Pittard <jeremy@jpittard.net>
2025-06-06 22:42:52 +0800

    nearly fleshed out landing page, brevo set up

---
+++ src/pages/api/sub.ts
@@ -0,0 +1,44 @@
+export const prerender = false;
+
+interface RequestParams {
+  email: string;
+  userTytpe: string;
+}
+
+export async function POST({ request }: any) {
+  try {
+    const body = await request.json();
+    const options = {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+        "api-key": import.meta.env.BAK,
+      },
+      body: JSON.stringify(body),
+    };
+    fetch("https://api.brevo.com/v3/contacts", options)
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err));
+    return new Response(JSON.stringify("woop"), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error: any) {
+    return new Response(
+      JSON.stringify({
+        error: "Gateway error",
+        message: error.message,
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+}
